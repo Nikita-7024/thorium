109ae68d494bac491fc728703245c983731430cd
Nikita-7024 <nikita.singh7024@gmail.com>
2022-03-03 02:23:11 +0530

    assignment 2 march

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,24 +1,36 @@
 const { count } = require("console")
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
     let book = req.body
+    let authorId = book.author
+    let publisherId = book.publisher
+
+    //validation a
+    if(!authorId) return res.send('Author details are required.')
+
+    //validation b
+    let author = await authorModel.findById(authorId)
+    if(!author) return res.send('No author is present with the given author id')
+
+    //validation c
+    if(!publisherId) return res.send('Publisher details are required.') 
+
+    //validation d
+    let publisher = await publisherModel.findById(publisherId)
+    if(!publisher) return res.send('No publisher is present with the given publisher id')
+
     let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+    return res.send({data: bookCreated})
 }
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+const getBooks= async function (req, res) {
+    let books = await bookModel.find().populate('author publisher')
     res.send({data: books})
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
-
-}
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.getBooks= getBooks
