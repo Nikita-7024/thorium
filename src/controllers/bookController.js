3da27a361977a9c94342a038b945ac08639aff54
Nikita-7024 <nikita.singh7024@gmail.com>
2022-03-04 01:05:15 +0530

    updated assignment ref&populate 3march

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -32,5 +32,58 @@ const getBooks= async function (req, res) {
 }
 
 
+const updateHardCover = async function (req, res){
+
+    let data = req.body
+
+    let books = await bookModel.find().populate("publisher")
+
+        
+    let booksByPublisher = books.filter(ele => (ele.publisher.name == "Penguin") || (ele.publisher.name == "HarperCollins")) 
+
+    let booksName = booksByPublisher.map(x => x.name)
+    // console.log(booksName)
+
+    let updatedCover = []
+
+    for (let i=0; i<booksName.length;  i++){
+        let element = booksName[i]
+        let updateData = await bookModel.findOneAndUpdate({name : element}, {$set : data}, {new : true})
+        updatedCover.push(updateData)
+    }
+    res.send({updatedCover : updatedCover})
+
+        
+
+}
+
+
+const updatePrice = async function (req,res) {
+
+   
+
+    let books = await bookModel.find().populate("author")
+
+    let authorsRating = books.filter(ele => (ele.author.rating >= 3.5))
+    
+    let booksName = authorsRating.map(x => x.name)
+    // console.log(booksName)
+    let updatedPrice = []
+
+    for (let i=0; i<booksName.length;  i++){
+        let element = booksName[i]
+        let updateData = await bookModel.findOneAndUpdate({name : element}, {$inc : {price : +10}}, {new : true})
+        updatedPrice.push(updateData)
+    }
+    res.send({updatedPrice : updatedPrice})
+    
+    
+   }
+
+
+
+
 module.exports.createBook= createBook
 module.exports.getBooks= getBooks
+module.exports.updateHardCover = updateHardCover
+module.exports.updatePrice = updatePrice
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,16 @@ const bookController = require("../controllers/bookController")
 const publisherController = require("../controllers/publisherController")
 
 router.post("/createAuthor", authorController.createAuthor  )
+router.get("/getAuthorsData", authorController.getAuthorsData)
+
 router.post("/createPublisher", publisherController.createPublisher)
+router.get("/getPublisherData", publisherController.getPublisherData)
+
 router.post("/createBook", bookController.createBook  )
 router.get("/getBooks", bookController.getBooks)
+
+router.put("/updateHardCoverData", bookController.updateHardCover)
+
+router.put("/updatePrice", bookController.updatePrice)
 
 module.exports = router;
