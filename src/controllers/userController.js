6561626f0a93315a0d0275bf548893187c417042
Nikita-7024 <nikita.singh7024@gmail.com>
2022-03-08 02:26:07 +0530

    assign 7 march

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,11 @@
-const UserModel= require("../models/userModel")
+const userModel= require("../models/userModel")
 
 const createUser= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
-    console.log(req.newAtribute)
-    res.send({msg: savedData})
+    let savedUser= await userModel.create(data)
+    res.send({msg: savedUser})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    console.log(req.newAtribute)
-    res.send({msg: allUsers})
-}
 
 module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+// module.exports.getUsersData= getUsersData
